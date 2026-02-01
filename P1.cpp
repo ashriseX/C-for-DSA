538e342d8f5090c1e139a61b0fff62083fa11fd2
ASH <ashrise.x@gmail.com>
2026-02-01 12:54:15 +0500

    First C++ program

---
+++ P1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "Hello Everyone!" << endl;
+    cout << "My name is Abdul Saboor.\n";
+    cout << "This is the start of my coding journey in C++ for DSA. I hope i learn it well" << endl;
+    return 0;
+
+}
+//Boilerplate code, this is the format mostly followed in the DSA problems in C++.

c422e8a96f2d903258c80a648153040a677828f5
ASH <ashrise.x@gmail.com>
2026-02-10 12:05:45 +0500

    extra

---
+++ P15.cpp
@@ -0,0 +1,11 @@
+//bitwise operators
+
+//& operator it works similar to the logical and operation in binary number
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 4, b=8;
+    cout << (a & b) << endl;
+    return 0;
+}

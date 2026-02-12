58792d10df6b6b6868022963d9805fff9b6e31c2
ASH <ashrise.x@gmail.com>
2026-02-13 00:16:13 +0500

    Streak

---
+++ P16 copy.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node *next;
+
+        //constructor for the data input
+        Node(int data){
+            this->data = data;
+            this->next = NULL;
+        }
+
+   
+
+};
+
+//List class for transversing through the node
+
+class List{
+    private:
+        Node *head;
+
+
+
+    public:    
+        //constructor to innitialize the list
+        List(){
+            head = NULL;
+
+        }
+     
+
+    //method to insert the data at the end of the list
+        void insertAtEnd(int data){
+            Node *nn = new Node(data);
+
+            if(head == NULL){
+                head = nn;
+            }
+            else{
+                Node *temp = head;
+                 
+                //loop for traversing the list
+                while(temp->next != NULL){
+                    temp = temp->next;
+                }
+                temp->next = nn;
+            
+            }
+        }
+    //method to add at the beginning
+
+        void insertAtStart(int data){
+            Node *nn = new Node(data);
+
+            nn->next = head;
+            head = nn;
+        }
+
+    //method to print the list
+        void displayList(){
+            Node *temp = head;
+            cout << "Start of the list:" << endl;
+            while(temp != NULL){
+                cout << " " << temp->data;
+                temp = temp->next;
+            }
+
+        cout << "\n End of the list." << endl;
+
+        return;
+
+        }
+
+    //destructor
+    ~List(){
+        Node *temp;
+
+        while(temp != NULL){
+            temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+};
+
+//main method for the test case
+int main(){
+
+    List *list = new List();
+
+    list->insertAtEnd(51);
+    list->insertAtEnd(24);
+    list->insertAtEnd(52345);
+    list->insertAtEnd(1);
+    list->insertAtEnd(5135);
+    list->insertAtEnd(325);
+
+    list->displayList();
+
+    list->insertAtStart(101);
+    list->displayList();
+
+    delete list;
+
+   
+    return 0;
+}

156d8d5e7b348d536f0458f1d3e04013a526dac9
ASH <ashrise.x@gmail.com>
2026-02-15 23:30:22 +0500

    Templates

--- P17.cpp
+++ P17.cpp
@@ -1,6 +1,53 @@
 #include <iostream>
 using namespace std;
 
+//using template which is similar to generics in java but more powerful
+
+template<typename T>
+ template<Node N>
+ 
+class Node{
+
+    T data;
+    Node *next;
+
+    public:
+        Node(){
+            this->data = 0;
+            this->next=NULL;
+        }
+
+        Node(T data){
+            this->data = data;
+            this->next = NULL;
+        }
+
+
+
+
+
+};
+
+//list class
+class List{
+    Node *head;
+    Node *tail;
+
+    public:
+        List(){
+            head = tail = NULL;
+
+        }
+        
+
+        void pushFront(int value){
+
+
+        }
+        void pushBack(){}
+
+};
+
 int main(){
 
     cout << "this is just to maintain the streak ";

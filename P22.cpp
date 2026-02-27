b2311e3d5c7d7394fdd2ca08298932e44178ebae
ashrise.X <ashrise.x@gmail.com>
2026-02-27 22:18:57 +0500

    C++ code from termux

---
+++ P22.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "First C++ program from termux";
+	
+	cout << "Enter a number:" << endl;
+	int num;
+	cin >> num;
+	cout << "this is your number: " << num << " other than this the program does nothing" << endl;
+
+	return 0;
+}
+
+
